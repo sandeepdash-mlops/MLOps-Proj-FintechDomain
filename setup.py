9d453907a5766c6b10855994ab9595d41b0bb9ee
root <root@instance-20250823-053318.asia-south1-b.c.isu-internal.internal>
2025-08-23 08:27:32 +0000

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sandeep Dash",
+    author_email="sandeepdashmlops@gmail.com",
+    packages=find_packages()
+)
